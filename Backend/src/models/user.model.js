56a05a6cfea221d12d97cc214a8b3a368ac79b87
Fahad Ali Babar <rajafahadali931@gmail.com>
2025-11-19 22:54:41 +0500

    JWT is in progress

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const UserSchema=new mongoose.Schema({
-    firstname:{
+    Username:{
         type:String,
         required:[true,"Name is required"],
         unique:true,
         lowercase:true,
         trim:true,
+        index:true,
         minlength: [5, "Username must be at least 5 characters"],
         maxlength: [30, "Username cannot exceed 30 characters"],
     },
-    
+    avatar:{
+        type:String,
+    },
     email:{
         type:String,
         required:[true, "Email is required"],
@@ -18,7 +23,20 @@ const UserSchema=new mongoose.Schema({
     },
     password:{
         type:String,
-        required:true
+        required:[true,"Passowrd is required"]
+    },
+    refreshtoken:{
+        type:String
     }
 },{timestamps:true})
+
+UserSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+    this.password=bcrypt.hash(this.password,12);
+    next();
+})
+UserSchema.methods.ispasswordCorrect=async function(password){
+   return await bcrypt.compare(password,this.password);
+
+}
 export const User=mongoose.model("User",UserSchema);
