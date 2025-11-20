9a1d0e05bc14937adf233e7d68f07086adcb00dd
Fahad Ali Babar <rajafahadali931@gmail.com>
2025-11-20 21:38:49 +0500

    file upload on cloudinary

---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({ 
+        CLOUD_NAME: process.env.CLOUD_NAME, 
+        API_KEY: process.env.API_KEY, 
+        API_SECRET: process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+    const cloudinaryfileupload=async(localfilepath)=>{
+        try {
+            if(!localfilepath) return null;
+            const fileuploadresponse=await cloudinary.uploader.upload(localfilepath,{
+                resource_type:"auto"
+            })
+            console.log("file has been uploaded on cloudinary successfully :", fileuploadresponse);
+            return fileuploadresponse
+            
+            
+        } catch (error) {
+            fs.unlinkSync(localfilepath)
+            return null;
+            
+        }
+
+    }
+    export {cloudinaryfileupload}
